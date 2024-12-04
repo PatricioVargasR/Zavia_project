1e3c14d21782f5c8c92450be971e7be6a37cc673
DaniVH <97567256+DaniVH7@users.noreply.github.com>
2024-12-04 15:09:09 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtoR6h351W-7mkAbKCWG6AqyjJbazzwqw",
+  authDomain: "zavia-5b1a3.firebaseapp.com",
+  projectId: "zavia-5b1a3",
+  storageBucket: "zavia-5b1a3.firebasestorage.app",
+  messagingSenderId: "100544919895",
+  appId: "1:100544919895:web:32757b79b50cccd8cda612"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
